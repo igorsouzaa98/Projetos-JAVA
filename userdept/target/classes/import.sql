694b016364277d4f56aa30664b6670ddea336d86
Igor Souza <92556719+igorsouzaa98@users.noreply.github.com>
2022-01-25 03:33:41 -0300

    API Rest Com Spring Boot
    Projeto de API Rest usando spring boot
    
    Um projeto que mostra o banco de dados de um local com dois departamentos.
    
    No projeto foi feito três endpoints Buscar o Usuário pelo ID, Adicionar Usuário e Listar todos os Usuários.

---
+++ userdept/target/classes/import.sql
@@ -0,0 +1,8 @@
+INSERT INTO tb_department(name) VALUES ('Gestão');
+INSERT INTO tb_department(name) VALUES ('Informática');
+
+INSERT INTO tb_user(department_id, name, email) VALUES (1, 'Roy', 'roy@gmail.com');
+INSERT INTO tb_user(department_id, name, email) VALUES (1, 'Jeff', 'jeff@gmail.com');
+INSERT INTO tb_user(department_id, name, email) VALUES (2, 'Igor', 'igor@gmail.com');
+INSERT INTO tb_user(department_id, name, email) VALUES (2, 'Lucas', 'lucas@gmail.com');
+INSERT INTO tb_user(department_id, name, email) VALUES (2, 'Jandu', 'jandu@gmail.com');
